c1eba57f23084696309a0c287e8a54ba2f6ac751
riquemath <fredabisai@gmail.com>
2020-11-12 15:00:37 +0300

    Create a separate all certificates section

---
+++ src/app/pages/home/all-certificates/all-certificates.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-certificates',
+  templateUrl: './all-certificates.component.html',
+  styleUrls: ['./all-certificates.component.css']
+})
+export class AllCertificatesComponent implements OnInit {
+  @Input() eventsLoading;
+  @Input() eventsAnalytics;
+  searchText = '';
+  page = 1;
+  itemsPerPage = 10;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  searchingItems(e) {
+    if (e) {
+      e.stopPropagation();
+    }
+    this.searchText = e ? e.target.value.trim() : this.searchText;
+  }
+  trackByFn(index, item) {
+    return item.id;
+  }
+  onUpdatePageSize(e) {
+    this.itemsPerPage = e;
+  }
+
+  onCurrentPageUpdate(e) {
+    this.page = e;
+  }
+
+
+
+}
