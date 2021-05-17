64a5c48c75e1dc0c08a92ae6c16b0b0798585dc6
riquemath <fredabisai@gmail.com>
2021-05-17 11:26:40 +0300

    Upgrade app to version 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,9 +22,10 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      useHash: true,
-      preloadingStrategy: PreloadAllModules,
-    }),
+    useHash: true,
+    preloadingStrategy: PreloadAllModules,
+    relativeLinkResolution: 'legacy'
+}),
   ],
   exports: [RouterModule],
 })
